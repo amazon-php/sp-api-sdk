1c310c0757205d224e1cce9b6a2399d411b1279a
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-11-23 15:07:51 +0100

    Updated SDK (#92)

--- src/AmazonPHP/SellingPartner/Api/ProductTypesDefinitionsSDK.php
+++ src/AmazonPHP/SellingPartner/Api/ProductTypesDefinitionsSDK.php
@@ -51,7 +51,7 @@ final class ProductTypesDefinitionsSDK
      *
      * @param AccessToken $accessToken
      * @param string $product_type The Amazon product type name. (required)
-     * @param string[] $marketplace_ids A comma-delimited list of Amazon marketplace identifiers for the request. (required)
+     * @param string[] $marketplace_ids A comma-delimited list of Amazon marketplace identifiers for the request. Note: This parameter is limited to one marketplaceId at this time. (required)
      * @param string $seller_id A selling partner identifier. When provided, seller-specific requirements and values are populated within the product type definition schema, such as brand names associated with the selling partner. (optional)
      * @param string $product_type_version The version of the Amazon product type to retrieve. Defaults to \&quot;LATEST\&quot;,. Prerelease versions of product type definitions may be retrieved with \&quot;RELEASE_CANDIDATE\&quot;. If no prerelease version is currently available, the \&quot;LATEST\&quot; live version will be provided. (optional, default to 'LATEST')
      * @param string $requirements The name of the requirements set to retrieve requirements for. (optional, default to 'LISTING')
@@ -153,7 +153,7 @@ final class ProductTypesDefinitionsSDK
      *
      * @param AccessToken $accessToken
      * @param string $product_type The Amazon product type name. (required)
-     * @param string[] $marketplace_ids A comma-delimited list of Amazon marketplace identifiers for the request. (required)
+     * @param string[] $marketplace_ids A comma-delimited list of Amazon marketplace identifiers for the request. Note: This parameter is limited to one marketplaceId at this time. (required)
      * @param string $seller_id A selling partner identifier. When provided, seller-specific requirements and values are populated within the product type definition schema, such as brand names associated with the selling partner. (optional)
      * @param string $product_type_version The version of the Amazon product type to retrieve. Defaults to \&quot;LATEST\&quot;,. Prerelease versions of product type definitions may be retrieved with \&quot;RELEASE_CANDIDATE\&quot;. If no prerelease version is currently available, the \&quot;LATEST\&quot; live version will be provided. (optional, default to 'LATEST')
      * @param string $requirements The name of the requirements set to retrieve requirements for. (optional, default to 'LISTING')
