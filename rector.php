5559c18165e5826d2078cd9b52926cca4b338aa5
Norbert Orzechowicz <norbert@orzechowicz.pl>
2021-12-28 17:56:22 +0100

    Add custom rector rules to fix types in parameters automatically mapped to objects

--- rector.php
+++ rector.php
@@ -2,10 +2,22 @@
 
 declare(strict_types=1);
 
+use AmazonPHP\SellingPartner\Model\CatalogItem\Categories;
+use AmazonPHP\SellingPartner\Model\ListingsItems\ListingsItemPutRequest;
+use AmazonPHP\SellingPartner\Model\Messaging\GetSchemaResponse;
+use AmazonPHP\SellingPartner\Model\Uploads\UploadDestination;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\NullType;
+use PHPStan\Type\UnionType;
 use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\TypeDeclaration\ValueObject\AddParamTypeDeclaration;
+use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
@@ -32,4 +44,63 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(SetList::PHP_73);
     $containerConfigurator->import(SetList::PHP_74);
     $containerConfigurator->import(SetList::TYPE_DECLARATION);
+
+    /**
+     * Explanation here: https://github.com/amazon-php/sp-api-sdk/issues/101#issuecomment-1002159988
+     */
+    $services->set(AddParamTypeDeclarationRector::class)
+        ->call('configure', [[
+            AddParamTypeDeclarationRector::PARAMETER_TYPEHINTS => ValueObjectInliner::inline([
+                new AddParamTypeDeclaration(
+                    Categories::class,
+                    'setParent',
+                    0,
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())])
+                ),
+                new AddParamTypeDeclaration(
+                    ListingsItemPutRequest::class,
+                    'setAttributes',
+                    0,
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())])
+                ),
+                new AddParamTypeDeclaration(
+                    GetSchemaResponse::class,
+                    'setPayload',
+                    0,
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())])
+                ),
+                new AddParamTypeDeclaration(
+                    UploadDestination::class,
+                    'setHeaders',
+                    0,
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())])
+                ),
+            ])
+        ]]);
+
+    $services->set(AddReturnTypeDeclarationRector::class)
+        ->call('configure', [[
+            AddReturnTypeDeclarationRector::METHOD_RETURN_TYPES => ValueObjectInliner::inline([
+                new AddReturnTypeDeclaration(
+                    Categories::class,
+                    'getParent',
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())]),
+                ),
+                new AddReturnTypeDeclaration(
+                    ListingsItemPutRequest::class,
+                    'getAttributes',
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())]),
+                ),
+                new AddReturnTypeDeclaration(
+                    GetSchemaResponse::class,
+                    'getPayload',
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())]),
+                ),
+                new AddReturnTypeDeclaration(
+                    GetSchemaResponse::class,
+                    'getHeaders',
+                    new UnionType([new NullType(), new ArrayType(new MixedType(), new MixedType())]),
+                ),
+            ])
+        ]]);
 };
