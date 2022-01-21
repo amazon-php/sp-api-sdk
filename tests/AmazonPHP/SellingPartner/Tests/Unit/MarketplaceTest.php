e9be8db42cf7c422bb313f780394fe7aa7f1ee18
Jason Hebert <jason.hebert@powersrc.net>
2022-01-21 17:01:10 -0600

    Refactor AmazonPHP/SellingPartner/Marketplace class
    Refactor Marketplace class to remove redundancies and simplify update process.
    Correct namespace on ObjectSerializerTest class.

---
+++ tests/AmazonPHP/SellingPartner/Tests/Unit/MarketplaceTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AmazonPHP\Test\AmazonPHP\SellingPartner\Tests\Unit;
+
+use AmazonPHP\SellingPartner\Exception\InvalidArgumentException;
+use AmazonPHP\SellingPartner\Marketplace;
+use BadMethodCallException;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use function array_keys;
+
+final class MarketplaceTest extends TestCase
+{
+    private array $countries;
+
+    protected function setUp(): void
+    {
+        $this->countries = $this->getMarketplaceCountries();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->countries = [];
+    }
+
+    public function test_initialization_from_static_methods(): void
+    {
+        foreach ($this->countries as $country) {
+            $this->assertInstanceOf(Marketplace::class, Marketplace::$country());
+        }
+
+        $this->expectException(BadMethodCallException::class);
+        Marketplace::XX();
+    }
+
+    public function test_initialization_from_country_code(): void
+    {
+        foreach ($this->countries as $country) {
+            $this->assertInstanceOf(Marketplace::class, Marketplace::fromCountry($country));
+        }
+
+        $this->expectException(InvalidArgumentException::class);
+        Marketplace::fromCountry('XX');
+    }
+
+    public function test_all_method_returns_correct_marketplaces(): void
+    {
+        $all = Marketplace::all();
+
+        $this->assertIsArray($all);
+        $this->assertCount(count($this->countries), $all);
+
+        foreach ($all as $marketplace) {
+            $this->assertInstanceOf(Marketplace::class, $marketplace);
+        }
+    }
+
+    private function getMarketplaceCountries(): array
+    {
+        $class = new ReflectionClass(Marketplace::class);
+
+        return array_keys($class->getStaticPropertyValue('countryMap'));
+    }
+}
