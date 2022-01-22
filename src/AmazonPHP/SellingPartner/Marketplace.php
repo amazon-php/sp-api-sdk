65fb3fb5fca6bc02c680f82c29f709a4d0715eda
Jason Hebert <4276690+jasonhebert@users.noreply.github.com>
2022-01-22 02:36:58 -0600

    Refactor AmazonPHP/SellingPartner/Marketplace class (#128)
    * Refactor AmazonPHP/SellingPartner/Marketplace class
    
    Refactor Marketplace class to remove redundancies and simplify update process.
    Correct namespace on ObjectSerializerTest class.
    
    * Fix code style

--- src/AmazonPHP/SellingPartner/Marketplace.php
+++ src/AmazonPHP/SellingPartner/Marketplace.php
@@ -4,11 +4,148 @@ declare(strict_types=1);
 
 namespace AmazonPHP\SellingPartner;
 
-use AmazonPHP\SellingPartner\Exception\Exception;
 use AmazonPHP\SellingPartner\Exception\InvalidArgumentException;
 
+/**
+ * @method static CA()
+ * @method static US()
+ * @method static MX()
+ * @method static BR()
+ * @method static ES()
+ * @method static GB()
+ * @method static FR()
+ * @method static NL()
+ * @method static DE()
+ * @method static IT()
+ * @method static SE()
+ * @method static PL()
+ * @method static EG()
+ * @method static TR()
+ * @method static AE()
+ * @method static IN()
+ * @method static SG()
+ * @method static AU()
+ * @method static JP()
+ */
 final class Marketplace
 {
+    private static array $countryMap = [
+        'CA' => [
+            'name'   => 'Canada',
+            'id'     => 'A2EUQ1WTGCTBG2',
+            'region' => Regions::NORTH_AMERICA,
+            'url'    => 'https://sellercentral.amazon.ca',
+        ],
+        'US' => [
+            'name'   => 'United States of America',
+            'id'     => 'ATVPDKIKX0DER',
+            'region' => Regions::NORTH_AMERICA,
+            'url'    => 'https://sellercentral.amazon.com',
+        ],
+        'MX' => [
+            'name'   => 'Mexico',
+            'id'     => 'A1AM78C64UM0Y8',
+            'region' => Regions::NORTH_AMERICA,
+            'url'    => 'https://sellercentral.amazon.com.mx',
+        ],
+        'BR' => [
+            'name'   => 'Brazil',
+            'id'     => 'A2Q3Y263D00KWC',
+            'region' => Regions::NORTH_AMERICA,
+            'url'    => 'https://sellercentral.amazon.com.br',
+        ],
+        'ES' => [
+            'name'   => 'Spain',
+            'id'     => 'A1RKKUPIHCS9HS',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral-europe.amazon.com',
+        ],
+        'GB' => [
+            'name'   => 'United Kingdom',
+            'id'     => 'A1F83G8C2ARO7P',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral-europe.amazon.com',
+        ],
+        'FR' => [
+            'name'   => 'France',
+            'id'     => 'A13V1IB3VIYZZH',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral-europe.amazon.com',
+        ],
+        'NL' => [
+            'name'   => 'Netherlands',
+            'id'     => 'A1805IZSGTT6HS',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.nl',
+        ],
+        'DE' => [
+            'name'   => 'Germany',
+            'id'     => 'A1PA6795UKMFR9',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral-europe.amazon.com',
+        ],
+        'IT' => [
+            'name'   => 'Italy',
+            'id'     => 'APJ6JRA9NG5V4',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral-europe.amazon.com',
+        ],
+        'SE' => [
+            'name'   => 'Sweden',
+            'id'     => 'A2NODRKZP88ZB9',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.se',
+        ],
+        'PL' => [
+            'name'   => 'Poland',
+            'id'     => 'A1C3SOZRARQ6R3',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.pl',
+        ],
+        'EG' => [
+            'name'   => 'Egypt',
+            'id'     => 'ARBP9OOSHTCHU',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.eg',
+        ],
+        'TR' => [
+            'name'   => 'Turkey',
+            'id'     => 'A33AVAJ2PDY3EV',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.com.tr',
+        ],
+        'AE' => [
+            'name'   => 'United Arab Emirates',
+            'id'     => 'A2VIGQ35RCS4UG',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.ae',
+        ],
+        'IN' => [
+            'name'   => 'India',
+            'id'     => 'A21TJRUUN4KGV',
+            'region' => Regions::EUROPE,
+            'url'    => 'https://sellercentral.amazon.in',
+        ],
+        'SG' => [
+            'name'   => 'Singapore',
+            'id'     => 'A19VAU5U5O7RUS',
+            'region' => Regions::FAR_EAST,
+            'url'    => 'https://sellercentral.amazon.sg',
+        ],
+        'AU' => [
+            'name'   => 'Australia',
+            'id'     => 'A39IBJ37TRP1C6',
+            'region' => Regions::FAR_EAST,
+            'url'    => 'https://sellercentral.amazon.com.au',
+        ],
+        'JP' => [
+            'name'   => 'Japan',
+            'id'     => 'A1VC38T7YXB528',
+            'region' => Regions::FAR_EAST,
+            'url'    => 'https://sellercentral.amazon.co.jp',
+        ],
+    ];
+
     private string $name;
 
     private string $id;
@@ -17,41 +154,29 @@ final class Marketplace
 
     private string $region;
 
-    private function __construct(string $name, string $id, string $countryCode, string $region)
+    private string $sellerCentralUrl;
+
+    private function __construct(string $name, string $id, string $countryCode, string $region, string $sellerCentralUrl)
     {
         $this->name = $name;
         $this->id = $id;
         $this->countryCode = $countryCode;
         $this->region = $region;
+        $this->sellerCentralUrl = $sellerCentralUrl;
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public static function fromCountry(string $countryCode) : self
     {
         $countryCode = \strtoupper($countryCode);
 
-        switch ($countryCode) {
-            case 'CA':
-            case 'US':
-            case 'MX':
-            case 'BR':
-            case 'ES':
-            case 'GB':
-            case 'FR':
-            case 'NL':
-            case 'DE':
-            case 'IT':
-            case 'SE':
-            case 'PL':
-            case 'TR':
-            case 'AE':
-            case 'IN':
-            case 'SG':
-            case 'AU':
-            case 'JP':
-                return self::$countryCode();
+        try {
+            return self::$countryCode();
+        } catch (\BadMethodCallException $e) {
+            throw new InvalidArgumentException("Unexpected country code {$countryCode}");
         }
-
-        throw new InvalidArgumentException("Unexpected country code {$countryCode}");
     }
 
     /**
@@ -59,121 +184,24 @@ final class Marketplace
      */
     public static function all() : array
     {
-        return [
-            self::CA(),
-            self::US(),
-            self::MX(),
-            self::BR(),
-            self::ES(),
-            self::GB(),
-            self::FR(),
-            self::NL(),
-            self::DE(),
-            self::IT(),
-            self::SE(),
-            self::PL(),
-            self::TR(),
-            self::AE(),
-            self::IN(),
-            self::SG(),
-            self::AU(),
-            self::JP(),
-        ];
+        $marketplaces = [];
+
+        foreach (self::$countryMap as $countryCode => $item) {
+            $marketplaces[] = new self($item['name'], $item['id'], $countryCode, $item['region'], $item['url']);
+        }
+
+        return $marketplaces;
     }
 
-    public static function US() : self
+    public static function __callStatic(string $country, array $parameters)
     {
-        return new self('United States of America', 'ATVPDKIKX0DER', 'US', Regions::NORTH_AMERICA);
-    }
+        if (!\array_key_exists($country, self::$countryMap)) {
+            throw new \BadMethodCallException('Call to undefined method ' . self::class . '::' . $country . '()');
+        }
 
-    public static function CA() : self
-    {
-        return new self('Canada', 'A2EUQ1WTGCTBG2', 'CA', Regions::NORTH_AMERICA);
-    }
+        $map = self::$countryMap[$country];
 
-    public static function MX() : self
-    {
-        return new self('Mexico', 'A1AM78C64UM0Y8', 'MX', Regions::NORTH_AMERICA);
-    }
-
-    public static function BR() : self
-    {
-        return new self('Brazil', 'A2Q3Y263D00KWC', 'BR', Regions::NORTH_AMERICA);
-    }
-
-    public static function ES() : self
-    {
-        return new self('Spain', 'A1RKKUPIHCS9HS', 'ES', Regions::EUROPE);
-    }
-
-    public static function GB() : self
-    {
-        return new self('United Kingdom', 'A1F83G8C2ARO7P', 'GB', Regions::EUROPE);
-    }
-
-    public static function FR() : self
-    {
-        return new self('France', 'A13V1IB3VIYZZH', 'FR', Regions::EUROPE);
-    }
-
-    public static function NL() : self
-    {
-        return new self('Netherlands', 'A1805IZSGTT6HS', 'NL', Regions::EUROPE);
-    }
-
-    public static function DE() : self
-    {
-        return new self('Germany', 'A1PA6795UKMFR9', 'DE', Regions::EUROPE);
-    }
-
-    public static function IT() : self
-    {
-        return new self('Italy', 'APJ6JRA9NG5V4', 'IT', Regions::EUROPE);
-    }
-
-    public static function SE() : self
-    {
-        return new self('Sweden', 'A2NODRKZP88ZB9', 'SE', Regions::EUROPE);
-    }
-
-    public static function PL() : self
-    {
-        return new self('Poland', 'A1C3SOZRARQ6R3', 'PL', Regions::EUROPE);
-    }
-
-    public static function EG() : self
-    {
-        return new self('Egypt', 'ARBP9OOSHTCHU', 'EG', Regions::EUROPE);
-    }
-
-    public static function TR() : self
-    {
-        return new self('Turkey', 'A33AVAJ2PDY3EV', 'TR', Regions::EUROPE);
-    }
-
-    public static function AE() : self
-    {
-        return new self('United Arab Emirates', 'A2VIGQ35RCS4UG', 'AE', Regions::EUROPE);
-    }
-
-    public static function IN() : self
-    {
-        return new self('India', 'A21TJRUUN4KGV', 'IN', Regions::EUROPE);
-    }
-
-    public static function SG() : self
-    {
-        return new self('Singapore', 'A19VAU5U5O7RUS', 'SG', Regions::FAR_EAST);
-    }
-
-    public static function AU() : self
-    {
-        return new self('Australia', 'A39IBJ37TRP1C6', 'AU', Regions::FAR_EAST);
-    }
-
-    public static function JP() : self
-    {
-        return new self('Japan', 'A1VC38T7YXB528', 'JP', Regions::FAR_EAST);
+        return new self($map['name'], $map['id'], $country, $map['region'], $map['url']);
     }
 
     public function name() : string
@@ -198,42 +226,6 @@ final class Marketplace
 
     public function sellerCentralUrl() : string
     {
-        switch ($this->countryCode) {
-            case 'CA':
-                return 'https://sellercentral.amazon.ca';
-            case 'US':
-                return 'https://sellercentral.amazon.com';
-            case 'MX':
-                return 'https://sellercentral.amazon.com.mx';
-            case 'BR':
-                return 'https://sellercentral.amazon.com.br';
-            case 'ES':
-            case 'GB':
-            case 'FR':
-                return 'https://sellercentral-europe.amazon.com';
-            case 'NL':
-                return 'https://sellercentral.amazon.nl';
-            case 'DE':
-            case 'IT':
-                return 'https://sellercentral-europe.amazon.com';
-            case 'SE':
-                return 'https://sellercentral.amazon.se';
-            case 'PL':
-                return 'https://sellercentral.amazon.pl';
-            case 'TR':
-                return 'https://sellercentral.amazon.com.tr';
-            case 'AE':
-                return 'https://sellercentral.amazon.ae';
-            case 'IN':
-                return 'https://sellercentral.amazon.in';
-            case 'SG':
-                return 'https://sellercentral.amazon.sg';
-            case 'AU':
-                return 'https://sellercentral.amazon.com.au';
-            case 'JP':
-                return 'https://sellercentral.amazon.co.jp';
-        }
-
-        throw new Exception("Unexpected country code {$this->countryCode}");
+        return $this->sellerCentralUrl;
     }
 }
