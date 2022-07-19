bec172a489904d5883ea5e8a7ecfc2ee42bb1c50
Łukasz Owsiak <owsiakl@outlook.com>
2022-07-19 17:32:11 +0200

    Changed date time format during deserialization

--- tests/AmazonPHP/SellingPartner/Tests/Functional/FulfillmentOutboundApiTest.php
+++ tests/AmazonPHP/SellingPartner/Tests/Functional/FulfillmentOutboundApiTest.php
@@ -6,10 +6,15 @@ namespace AmazonPHP\Test\AmazonPHP\SellingPartner\Tests\Functional;
 
 use AmazonPHP\SellingPartner\Marketplace;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\Address;
+use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\CODSettings;
+use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\CreateFulfillmentOrderItem;
+use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\CreateFulfillmentOrderRequest;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\FeatureSettings;
+use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\FulfillmentAction;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\FulfillmentPreview;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\GetFulfillmentPreviewItem;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\GetFulfillmentPreviewRequest;
+use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\Money;
 use AmazonPHP\SellingPartner\Model\FulfillmentOutbound\ShippingSpeedCategory;
 
 final class FulfillmentOutboundApiTest extends SandboxTestCase
@@ -80,5 +85,56 @@ final class FulfillmentOutboundApiTest extends SandboxTestCase
 
         $this->assertCount(1, $response->getPayload()->getFulfillmentPreviews());
         $this->assertContainsOnlyInstancesOf(FulfillmentPreview::class, $response->getPayload()->getFulfillmentPreviews());
+    }
+
+    public function test_sandbox_create_fulfillment_order() : void
+    {
+        $marketplace = Marketplace::US();
+
+        $response = $this->sellingPartnerSDK->fulfillmentOutbound()->createFulfillmentOrder(
+            $this->sellingPartnerSDK->oAuth()->exchangeRefreshToken($this->sellerRefreshToken),
+            $marketplace->region(),
+            new CreateFulfillmentOrderRequest([
+                'marketplace_id' => 'ATVPDKIKX0DER',
+                'seller_fulfillment_order_id' => 'FBATestOrder-11',
+                'displayable_order_id' => 'TestOrder-FBAOutbound',
+                'displayable_order_date' => new \DateTime('2020-01-09T19:46:45.809Z'),
+                'displayable_order_comment' => 'TestOrder',
+                'shipping_speed_category' => new ShippingSpeedCategory(ShippingSpeedCategory::STANDARD),
+                'destination_address' => new Address([
+                    'name' => 'Amazon',
+                    'address_line1' => '1234 Amazon Way',
+                    'address_line2' => 'Suite 123',
+                    'address_line3' => 'Lane1',
+                    'city' => 'Troy',
+                    'state_or_region' => 'MI',
+                    'postal_code' => '48084',
+                    'country_code' => 'US',
+                ]),
+                'fulfillment_action' => new FulfillmentAction(FulfillmentAction::SHIP),
+                'cod_settings' => new CODSettings([
+                    'is_cod_required' => false,
+                    'cod_charge' => new Money(['value' => '10.00', 'currency_code' => 'USD']),
+                    'cod_charge_tax' => new Money(['value' => '2.00', 'currency_code' => 'USD']),
+                    'shipping_charge' => new Money(['value' => '5.00', 'currency_code' => 'USD']),
+                    'shipping_charge_tax' => new Money(['value' => '3.00', 'currency_code' => 'USD']),
+                ]),
+                'notification_emails' => ['test1%40amazon.com', 'test2%40amazon.com'],
+                'items' => [
+                    new CreateFulfillmentOrderItem([
+                        'seller_sku' => 'CR-47K6-H6QN',
+                        'seller_fulfillment_order_item_id' => 'OrderItemID1',
+                        'quantity' => 3,
+                    ]),
+                    new CreateFulfillmentOrderItem([
+                        'seller_sku' => 'PSMM-TEST-SKU-Jan-21_19_59_44-0738',
+                        'seller_fulfillment_order_item_id' => 'OrderItemID2',
+                        'quantity' => 1,
+                    ])
+                ],
+            ])
+        );
+
+        $this->assertNull($response->getErrors());
     }
 }
--- tests/AmazonPHP/SellingPartner/Tests/Functional/ReportsApiTest.php
+++ tests/AmazonPHP/SellingPartner/Tests/Functional/ReportsApiTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AmazonPHP\Test\AmazonPHP\SellingPartner\Tests\Functional;
 
 use AmazonPHP\SellingPartner\Marketplace;
+use AmazonPHP\SellingPartner\Model\Reports\CreateReportScheduleSpecification;
 use AmazonPHP\SellingPartner\Model\Reports\Report;
 
 final class ReportsApiTest extends SandboxTestCase
@@ -22,5 +23,23 @@ final class ReportsApiTest extends SandboxTestCase
 
         $this->assertCount(1, $response->getReports());
         $this->assertContainsOnlyInstancesOf(Report::class, $response->getReports());
+    }
+
+    public function test_sandbox_create_report_schedule() : void
+    {
+        $marketplace = Marketplace::US();
+
+        $response = $this->sellingPartnerSDK->reports()->createReportSchedule(
+            $this->sellingPartnerSDK->oAuth()->exchangeRefreshToken($this->sellerRefreshToken),
+            $marketplace->region(),
+            new CreateReportScheduleSpecification([
+                'report_type' => 'FEE_DISCOUNTS_REPORT',
+                'marketplace_ids' => ['A1PA6795UKMFR9', 'ATVPDKIKX0DER'],
+                'period' => 'PT5M',
+                'next_report_creation_time' => new \DateTime('2019-12-10T20:11:24.000Z'),
+            ])
+        );
+
+        $this->assertEquals("ID323", $response->getReportScheduleId());
     }
 }
