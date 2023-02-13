935bfceb2153463865bac0e23cfa391c35881a83
Norbert Orzechowicz <norbert@orzechowicz.pl>
2023-02-13 11:52:36 +0100

    Fixed creating http signature for queries with spaces

--- src/AmazonPHP/SellingPartner/HttpSignatureHeaders.php
+++ src/AmazonPHP/SellingPartner/HttpSignatureHeaders.php
@@ -93,13 +93,10 @@ final class HttpSignatureHeaders
         //Prepare credentials scope
         $credentialScope = $shortDate . '/' . $region . '/' . $service . '/aws4_request';
 
-        \parse_str($request->getUri()->getQuery(), $queryElements);
-        \ksort($queryElements);
-
         //prepare canonical request
         $canonicalString = $request->getMethod()
-            . "\n" . $request->getUri()->getPath()
-            . "\n" . \http_build_query($queryElements, '', '&', PHP_QUERY_RFC3986)
+            . "\n" . self::createCanonicalizedPath($request->getUri()->getPath())
+            . "\n" . self::build(self::parseUrl($request->getUri()->getQuery()))
             . "\n" . $canonicalHeadersStr
             . "\n" . $signedHeadersStr
             . "\n" . $hashedPayload;
@@ -226,13 +223,10 @@ final class HttpSignatureHeaders
         //Prepare credentials scope
         $credentialScope = $shortDate . '/' . $region . '/' . $service . '/aws4_request';
 
-        \parse_str($request->getUri()->getQuery(), $queryElements);
-        \ksort($queryElements);
-
         //prepare canonical request
         $canonicalString = $request->getMethod()
-            . "\n" . $request->getUri()->getPath()
-            . "\n" . \http_build_query($queryElements, '', '&', PHP_QUERY_RFC3986)
+            . "\n" . self::createCanonicalizedPath($request->getUri()->getPath())
+            . "\n" . self::build(self::parseUrl($request->getUri()->getQuery()))
             . "\n" . $canonicalHeadersStr
             . "\n" . $signedHeadersStr
             . "\n" . $hashedPayload;
@@ -266,5 +260,108 @@ final class HttpSignatureHeaders
         return $request
             ->withHeader('x-amz-date', $amzdate)
             ->withHeader('Authorization', $authorizationHeader);
+    }
+
+    /**
+     * Taken from \GuzzleHttp\Psr7\Query.
+     */
+    public static function parseUrl(string $str, $urlEncoding = true) : array
+    {
+        $result = [];
+
+        if ($str === '') {
+            return $result;
+        }
+
+        if ($urlEncoding === true) {
+            $decoder = function ($value) {
+                return \rawurldecode(\str_replace('+', ' ', (string) $value));
+            };
+        } elseif ($urlEncoding === PHP_QUERY_RFC3986) {
+            $decoder = 'rawurldecode';
+        } elseif ($urlEncoding === PHP_QUERY_RFC1738) {
+            $decoder = 'urldecode';
+        } else {
+            $decoder = function ($str) {
+                return $str;
+            };
+        }
+
+        foreach (\explode('&', $str) as $kvp) {
+            $parts = \explode('=', $kvp, 2);
+            $key = $decoder($parts[0]);
+            $value = isset($parts[1]) ? $decoder($parts[1]) : null;
+
+            if (!\array_key_exists($key, $result)) {
+                $result[$key] = $value;
+            } else {
+                if (!\is_array($result[$key])) {
+                    $result[$key] = [$result[$key]];
+                }
+                $result[$key][] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Taken from \GuzzleHttp\Psr7\Query.
+     */
+    public static function build(array $params, $encoding = PHP_QUERY_RFC3986) : string
+    {
+        if (!$params) {
+            return '';
+        }
+
+        if ($encoding === false) {
+            $encoder = function (string $str) : string {
+                return $str;
+            };
+        } elseif ($encoding === PHP_QUERY_RFC3986) {
+            $encoder = 'rawurlencode';
+        } elseif ($encoding === PHP_QUERY_RFC1738) {
+            $encoder = 'urlencode';
+        } else {
+            throw new \InvalidArgumentException('Invalid type');
+        }
+
+        $qs = '';
+
+        foreach ($params as $k => $v) {
+            $k = $encoder((string) $k);
+
+            if (!\is_array($v)) {
+                $qs .= $k;
+                $v = \is_bool($v) ? (int) $v : $v;
+
+                if ($v !== null) {
+                    $qs .= '=' . $encoder((string) $v);
+                }
+                $qs .= '&';
+            } else {
+                foreach ($v as $vv) {
+                    $qs .= $k;
+                    $vv = \is_bool($vv) ? (int) $vv : $vv;
+
+                    if ($vv !== null) {
+                        $qs .= '=' . $encoder((string) $vv);
+                    }
+                    $qs .= '&';
+                }
+            }
+        }
+
+        return $qs ? (string) \substr($qs, 0, -1) : '';
+    }
+
+    /**
+     * Taken from \Aws\Signature\SignatureV4.
+     */
+    public static function createCanonicalizedPath(string $path) : string
+    {
+        $doubleEncoded = \rawurlencode(\ltrim($path, '/'));
+
+        return '/' . \str_replace('%2F', '/', $doubleEncoded);
     }
 }
