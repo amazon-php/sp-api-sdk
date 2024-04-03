9d6ec31c8d6c9286b5963f5b4fa5e2950f9be4a5
peter <peter@sasha-lab.com>
2024-04-03 17:46:45 +0800

    update

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,67 +1,67 @@
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/aplus-content-api-model/aplusContent_2020-11-01.json \
+    -c /sp-api/config/generator-aplus.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
 #docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/aplus-content-api-model/aplusContent_2020-11-01.json \
-#    -c /sp-api/config/generator-aplus.yaml \
+#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/authorization-api-model/authorization.json \
+#    -c /sp-api/config/generator-authorization.yaml \
 #    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
 #    -o /sp-api \
 #    --language-specific-primitives \\DateTimeInterface \
 #    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-##docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-##    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/authorization-api-model/authorization.json \
-##    -c /sp-api/config/generator-authorization.yaml \
-##    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-##    -o /sp-api \
-##    --language-specific-primitives \\DateTimeInterface \
-##    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
+
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/catalog-items-api-model/catalogItems_2022-04-01.json \
+    --skip-validate-spec \
+    -c /sp-api/config/generator-catalog-item.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fulfillment-inbound-api-model/fulfillmentInboundv2024-03-20.json \
+    -c /sp-api/config/generator-fba-inbound.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fba-inventory-api-model/fbaInventory.json \
+    -c /sp-api/config/generator-fba-inventory.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
 #docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/catalog-items-api-model/catalogItems_2022-04-01.json \
-#    --skip-validate-spec \
-#    -c /sp-api/config/generator-catalog-item.yaml \
+#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fba-small-and-light-api-model/fbaSmallandLight.json \
+#    -c /sp-api/config/generator-fba-small-and-light.yaml \
 #    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
 #    -o /sp-api \
 #    --language-specific-primitives \\DateTimeInterface \
 #    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-#docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fulfillment-inbound-api-model/fulfillmentInboundv2024-03-20.json \
-#    -c /sp-api/config/generator-fba-inbound.yaml \
-#    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-#    -o /sp-api \
-#    --language-specific-primitives \\DateTimeInterface \
-#    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-#docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fba-inventory-api-model/fbaInventory.json \
-#    -c /sp-api/config/generator-fba-inventory.yaml \
-#    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-#    -o /sp-api \
-#    --language-specific-primitives \\DateTimeInterface \
-#    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-##docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-##    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fba-small-and-light-api-model/fbaSmallandLight.json \
-##    -c /sp-api/config/generator-fba-small-and-light.yaml \
-##    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-##    -o /sp-api \
-##    --language-specific-primitives \\DateTimeInterface \
-##    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-#docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/feeds-api-model/feeds_2021-06-30.json \
-#    -c /sp-api/config/generator-feeds.yaml \
-#    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-#    -o /sp-api \
-#    --language-specific-primitives \\DateTimeInterface \
-#    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
-#
-#docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
-#    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/finances-api-model/financesV0.json \
-#    -c /sp-api/config/generator-finances.yaml \
-#    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
-#    -o /sp-api \
-#    --language-specific-primitives \\DateTimeInterface \
-#    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/feeds-api-model/feeds_2021-06-30.json \
+    -c /sp-api/config/generator-feeds.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
+
+docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/finances-api-model/financesV0.json \
+    -c /sp-api/config/generator-finances.yaml \
+    --global-property models,apis,apiDocs=false,modelDocs=false,modelTests=false,apiTests=false,supportingFiles=false \
+    -o /sp-api \
+    --language-specific-primitives \\DateTimeInterface \
+    --type-mappings date=\\DateTimeInterface,Date=\\DateTimeInterface,DateTime=\\DateTimeInterface
 
 docker run --rm -v "${PWD}:/sp-api" openapitools/openapi-generator-cli generate \
     -i https://raw.githubusercontent.com/amzn/selling-partner-api-models/main/models/fulfillment-inbound-api-model/fulfillmentInboundV0.json \
